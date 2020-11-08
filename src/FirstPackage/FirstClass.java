08e1fe9ad1c415ca953fd0d4d7b3095bfda3538d
ozcankilic <o.kilic4426@gmail.com>
2020-11-08 09:35:07 -0800

    Fifth commit from local master

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -2,22 +2,23 @@ package FirstPackage;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("First commit from local master");
-		
-		System.out.println("Second commit from local master");
-		
-		System.out.println("Thirth commit from local master");
-		
-		System.out.println("First  commit from master branch");
-		
-		System.out.println("Second  commit from master branch");
-		
+		System.out.println("First commit from local master");		
+		System.out.println("Second commit from local master");		
+		System.out.println("Thirth commit from local master");		
+		System.out.println("First  commit from master branch");		
+		System.out.println("Second  commit from master branch");		
 		System.out.println("Thirth  commit from master branch");
 		
 
 		System.out.println("Here is a marge conflict I create local master");
 
 		System.out.println("Here is the conflict we have from remote master branch");
+		
+		
+		
+		
+		
+		System.out.println("Here is another conflict from local master branch");
 
 		
 	}
