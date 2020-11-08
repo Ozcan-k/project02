40fe4ea2cb113b9fe983bc09e2b279f1c7b37391
ozcankilic <o.kilic4426@gmail.com>
2020-11-08 08:09:21 -0800

    second commit from local master

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -2,7 +2,9 @@ package FirstPackage;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("");
+		System.out.println("First commit frome local master");
+		
+		System.out.println("Second commit frome local master");
 	}
 
 }
