4abbe0ef5cd60e52bf79776cb96cd1c82014b9c0
Ozcan Kilic <70051285+Ozcan-k@users.noreply.github.com>
2020-11-08 08:22:00 -0800

    first commit from remote master branch

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -2,11 +2,13 @@ package FirstPackage;
 
 public class FirstClass {
 	public static void main(String[] args) {
-		System.out.println("First commit frome local master");
+		System.out.println("First commit from local master");
 		
-		System.out.println("Second commit frome local master");
+		System.out.println("Second commit from local master");
 		
-		System.out.println("Thirth commit frome local master");
+		System.out.println("Thirth commit from local master");
+		
+		System.out.println("First  commit from master branch");
 	}
 
 }
